af603ee1596008e5f32992c8f68a6501ede456a0
Mateogas <dckarlmat@gmail.com>
2025-10-18 19:27:44 +0800

    Aded AboutScreen.kt

--- app/src/main/java/com/mobicom/s18/domanais/joshua/beybladextournamentmanager/AboutScreen.kt
+++ app/src/main/java/com/mobicom/s18/domanais/joshua/beybladextournamentmanager/AboutScreen.kt
@@ -6,16 +6,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -23,7 +20,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
