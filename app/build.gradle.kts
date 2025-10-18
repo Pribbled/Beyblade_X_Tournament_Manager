475f425818da7cfe6cb5b8baa11b67397400b26a
Migzmo <josemiguelespinosa74@gmail.com>
2025-10-18 17:28:12 +0800

    added notifications-screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.compose.material:material-icons-extended")
 }
